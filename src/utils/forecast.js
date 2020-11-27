20ff92d2702e9f330038cb664837ab04d75c210a
Sakari Heino <sakari.heino@teliacompany.com>
2020-11-27 13:30:48 +0200

    Forecast update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + 
-            " degrees out. But it feels like "+body.current.feelslike)
+            " degrees out. But it feels like "+body.current.feelslike+". Humidity is "+body.current.humidity+" %")
 
         }
     })
